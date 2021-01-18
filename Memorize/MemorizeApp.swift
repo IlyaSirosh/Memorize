532eeea1afadaa8780b467ebf29bd44ab63a4d3c
Ilya Sirosh <sirosh.ilya@gmail.com>
2021-01-18 14:05:46 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Illya Sirosh on 18.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
