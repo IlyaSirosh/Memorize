24ae7e7ed539ad5aea448573c677d9be4dd7f593
Ilya Sirosh <sirosh.ilya@gmail.com>
2021-01-27 23:17:25 +0200

    Refactoring MemoryGame.Card

--- Memorize/Model/MemoryGame.swift
+++ Memorize/Model/MemoryGame.swift
@@ -96,46 +96,51 @@ struct MemoryGame<CardContent: Equatable> {
             self.isFaceUp = isFaceUp
         }
         
-        //MARK: - Card bonus time
         var bonusTimeLimit: TimeInterval = 6
-        private var faceUpTime: TimeInterval {
-            if let lastFaceUpDate = self.lastFaceUpDate {
-                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
-            } else {
-                return pastFaceUpTime
-            }
-        }
         private var lastFaceUpDate: Date?
         private var pastFaceUpTime: TimeInterval = 0
-        
-        var bonusTimeRamaining: TimeInterval {
-            max(0, bonusTimeLimit - faceUpTime)
-        }
-        
-        var bonusRamaining: Double {
-            (bonusTimeLimit > 0 && bonusTimeRamaining > 0) ? bonusTimeRamaining / bonusTimeLimit : 0
-        }
-        
-        var hasEarnedBonus: Bool {
-            isMatched && bonusTimeRamaining > 0
-        }
-        
-        var isConsumingTime: Bool {
-            isFaceUp && !isMatched && bonusTimeRamaining > 0
-        }
-        
-
-        private mutating func startUsingBonusTime() {
-            if isConsumingTime, lastFaceUpDate == nil {
-                lastFaceUpDate = Date()
-            }
-        }
-        
-        private mutating func stopUsingBonusTime() {
-            pastFaceUpTime = faceUpTime
-            lastFaceUpDate = nil
-        }
+    
     }
 
 
+}
+
+extension MemoryGame.Card {
+    
+
+    private var faceUpTime: TimeInterval {
+        if let lastFaceUpDate = self.lastFaceUpDate {
+            return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+        } else {
+            return pastFaceUpTime
+        }
+    }
+    
+    var bonusTimeRamaining: TimeInterval {
+        max(0, bonusTimeLimit - faceUpTime)
+    }
+    
+    var bonusRamaining: Double {
+        (bonusTimeLimit > 0 && bonusTimeRamaining > 0) ? bonusTimeRamaining / bonusTimeLimit : 0
+    }
+    
+    var hasEarnedBonus: Bool {
+        isMatched && bonusTimeRamaining > 0
+    }
+    
+    var isConsumingTime: Bool {
+        isFaceUp && !isMatched && bonusTimeRamaining > 0
+    }
+    
+
+    private mutating func startUsingBonusTime() {
+        if isConsumingTime, lastFaceUpDate == nil {
+            lastFaceUpDate = Date()
+        }
+    }
+    
+    private mutating func stopUsingBonusTime() {
+        pastFaceUpTime = faceUpTime
+        lastFaceUpDate = nil
+    }
 }
